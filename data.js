330a8fdd1ab25e322771a270d248019cc570c1c5
EduCode597 <madriagajohneduard@gmail.com>
2025-11-26 04:00:42 +0800

    90%

--- data.js
+++ data.js
@@ -22,16 +22,104 @@ var family = [
     }
 ]
 
-// var family = ['Mother', 'Father', 'Sister', 'Brother'];
-        // var familyDescription = [
-        //     'A loving mother who always cares for her family.',
-        //     'A hardworking father who provides for his loved ones.',
-        //     'A supportive sister who is always there to help.',
-        //     'A playful brother who brings joy to the household.'
-        // ];
-        // var familyImages = [
-        //     "images/family/Mother.jpg",
-        //     "images/family/Father.jpg",
-        //     "images/family/Sister.jpg",
-        //     "images/family/brother.jpg"
-        // ];
+
+var friends = [
+    {
+        "name": "Gian",
+        "description": 'High school BFF',
+        "picture": "images/friends/Gian.jpg"
+    },
+    {
+        "name": "Ned",
+        "description": 'Gym Bro, Barkada',
+        "picture": "images/friends/ned.jpg"
+    },
+    {
+        "name": "Mel Laurence",
+        "description": 'Close friend, Barkada',
+        "picture": "images/friends/mel.jpg"
+    }
+    ,
+    {
+        "name": "Dave",
+        "description": 'Classmate, Barkada',
+        "picture": "images/friends/deyb.jpg"
+    },
+    {
+        "name": "Kenzy",
+        "description": 'Close Friend, Barkada',
+        "picture": "images/friends/kenzy.jpg"
+    }
+    ,
+    {
+        "name": "CJ",
+        "description": 'Close Friend, Barkada',
+        "picture": "images/friends/claine.jpg"
+    }
+    ,
+    {
+        "name": "JC",
+        "description": 'Gaming Duo, Barkada',
+        "picture": "images/friends/jc.jpg"
+    }
+    ,
+    {
+        "name": "Rayver",
+        "description": 'Basketball, Classmate',
+        "picture": "images/friends/rayver.jpg"
+    }
+    ,
+    {
+        "name": "Jerald",
+        "description": 'Basketball, Classmate',
+        "picture": "images/friends/jerald.jpg"
+    }
+    ,
+    {
+        "name": "Marc Justin",
+        "description": 'Close Friend, Basketball',
+        "picture": "images/friends/ilaw.jpg"
+    }
+    ,
+    {
+        "name": "Jay Arl",
+        "description": 'Classmate, Basketball',
+        "picture": "images/friends/agoy.jpg"
+    }
+    ,
+    {
+        "name": "Louie",
+        "description": 'Gym Bro, Basketball',
+        "picture": "images/friends/luwi.jpg"
+    }
+    
+]
+
+var colleagues = [
+    {
+        "name": "Denmar",
+        "description": 'Gives good vibes, and gets along with everyone.',
+        "picture": "images/colleagues/dinmar.jpg"
+    },
+    {
+        "name": "Ermel",
+        "description": 'Viberider, has a humorous personality, and a great listener.',
+        "picture": "images/colleagues/irmel.jpg"
+    },
+    {
+        "name": "James",
+        "description": 'Chill guy, loves to workout, and is also a team player.',
+        "picture": "images/colleagues/jems.jpg"
+    },
+    {
+        "name": "Darren",
+        "description": 'Friendly and approachable, makes work enjoyable.',
+        "picture": "images/colleagues/darin jay.jpg"
+    },
+    {
+        "name": "Ken",
+        "description": 'Has a weird sense of humor, but is a dependable colleague.',
+        "picture": "images/colleagues/ken.jpg"
+    }
+]
+
