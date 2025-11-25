68354430ec15d2c08505c225827fedcef6bcec2e
EduCode597 <madriagajohneduard@gmail.com>
2025-11-26 00:46:47 +0800

    goodnight

---
+++ data.js
@@ -0,0 +1,37 @@
+var family = [
+    {
+        "role": "Mother",
+        "description": 'An awesome loving mother who always cares for her family.',
+        "picture": "images/family/Mother.jpg"
+    },
+    {
+        "role": "Father",
+        "description": 'A hardworking father who provides for his loved ones.',
+        "picture": "images/family/Father.jpg"
+    },
+    {
+        "role": "Sister",
+        "description": 'A supportive sister who is always there willing to help.',
+        "picture": "images/family/Sister.jpg"
+    }
+    ,
+    {
+        "role": "Brother",
+        "description": 'A playful younger brother who brings joy to the household.',
+        "picture": "images/family/brother.jpg"
+    }
+]
+
+// var family = ['Mother', 'Father', 'Sister', 'Brother'];
+        // var familyDescription = [
+        //     'A loving mother who always cares for her family.',
+        //     'A hardworking father who provides for his loved ones.',
+        //     'A supportive sister who is always there to help.',
+        //     'A playful brother who brings joy to the household.'
+        // ];
+        // var familyImages = [
+        //     "images/family/Mother.jpg",
+        //     "images/family/Father.jpg",
+        //     "images/family/Sister.jpg",
+        //     "images/family/brother.jpg"
+        // ];
